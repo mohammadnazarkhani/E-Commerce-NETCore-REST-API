8d013214b297c1d9fe59cc19641b400b539744b4
mohammadnazarkhani <m.nazarkhani1380@gmail.com>
2024-11-06 21:13:54 +0330

    Add DbContext and define relationships between models in TondForooshContext

---
+++ TondForoosh/TondForoosh.Api/Data/TondForooshContext.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TondForoosh.Api.Models;
+
+namespace TondForoosh.Api.Data
+{
+    public class TondForooshContext : DbContext
+    {
+        public TondForooshContext(DbContextOptions<TondForooshContext> options) : base(options)
+        {
+        }
+
+        // DbSets for each model
+        public DbSet<User> Users { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+        public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+        public DbSet<CartItem> CartItems { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // User to ShoppingCart: One-to-One
+            modelBuilder.Entity<User>()
+                .HasOne(u => u.ShoppingCart)
+                .WithOne(sc => sc.User)
+                .HasForeignKey<ShoppingCart>(sc => sc.UserId);
+
+            // User to Orders: One-to-Many
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.Orders)
+                .WithOne(o => o.User)
+                .HasForeignKey(o => o.UserId);
+
+            // ProductCategory to Products: One-to-Many
+            modelBuilder.Entity<ProductCategory>()
+                .HasMany(pc => pc.Products)
+                .WithOne(p => p.ProductCategory)
+                .HasForeignKey(p => p.ProductCategoryId);
+
+            // ShoppingCart to CartItems: One-to-Many
+            modelBuilder.Entity<ShoppingCart>()
+                .HasMany(sc => sc.CartItems)
+                .WithOne(ci => ci.ShoppingCart)
+                .HasForeignKey(ci => ci.ShoppingCartId);
+
+            // Product to CartItems: One-to-Many
+            modelBuilder.Entity<Product>()
+                .HasMany(p => p.CartItems)
+                .WithOne(ci => ci.Product)
+                .HasForeignKey(ci => ci.ProductId);
+
+            // Order to OrderItems: One-to-Many
+            modelBuilder.Entity<Order>()
+                .HasMany(o => o.OrderItems)
+                .WithOne(oi => oi.Order)
+                .HasForeignKey(oi => oi.OrderId);
+
+            // Product to OrderItems: One-to-Many
+            modelBuilder.Entity<Product>()
+                .HasMany(p => p.OrderItems)
+                .WithOne(oi => oi.Product)
+                .HasForeignKey(oi => oi.ProductId);
+        }
+    }
+}
