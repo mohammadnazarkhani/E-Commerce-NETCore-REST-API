53706d71176137ab51c03b754003c084b9cf3fb3
mohammadnazarkhani <m.nazarkhani1380@gmail.com>
2024-11-09 23:39:56 +0330

    Implement IUpdatable interface in entities, refactor User entity to include UpdateEntity method, and add UserRole enum within the User entity class.

--- TondForoosh/TondForoosh.Api/Entities/User.cs
+++ TondForoosh/TondForoosh.Api/Entities/User.cs
@@ -1,5 +1,9 @@
-﻿namespace TondForoosh.Api.Entities
+﻿using TondForoosh.Api.Dtos.User;
+using TondForoosh.Api.Common;
+
+namespace TondForoosh.Api.Entities
 {
+    // Enum to define the roles of the user
     public enum UserRole
     {
         Admin,
@@ -7,27 +11,23 @@
         User
     }
 
-    public class User
+    public class User : IUpdatable<UpdateUserDto>
     {
         public int Id { get; set; }
-
-        // Username should always be required (non-nullable)
         public required string Username { get; set; }
-
-        // Password should be required (non-nullable), but will likely be handled as a hashed value
         public required string Password { get; set; }
-
-        // Role is required, indicating the user's role (Admin, Seller, User)
         public required UserRole Role { get; set; }
 
         // Navigation properties
-        // ShoppingCart can be null if a user does not have one, so it's nullable
         public ShoppingCart? ShoppingCart { get; set; }
-
-        // Orders list is a required relationship (user must have at least one order or none)
         public List<Order> Orders { get; set; } = new List<Order>();
-
-        // SellerProducts list is a required relationship (seller must have at least one product or none)
         public List<SellerProduct> SellerProducts { get; set; } = new List<SellerProduct>();
+
+        // Implementation of UpdateEntity method from IUpdatable interface
+        public void UpdateEntity(UpdateUserDto userDto)
+        {
+            Username = userDto.Username;
+            Role = userDto.Role;
+        }
     }
 }
