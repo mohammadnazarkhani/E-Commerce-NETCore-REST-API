9dac61c636bf537e8627998f7f1def5c8c22fb3e
mohammadnazarkhani <m.nazarkhani1380@gmail.com>
2025-03-05 22:34:10 +0330

    feat: Register data services in Program.cs for improved configuration

--- TondForooshApi/Program.cs
+++ TondForooshApi/Program.cs
@@ -4,6 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.RegisterApiServices();
+builder.Services.RegisterDataServices(builder.Configuration);
 
 var app = builder.Build();
 
