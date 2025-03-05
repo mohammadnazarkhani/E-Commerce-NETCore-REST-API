45e65e1c7c0d5c611c20c3d718e7b020e2af4653
mohammadnazarkhani <m.nazarkhani1380@gmail.com>
2025-03-05 23:37:16 +0330

    feat: Specify precision for Product price in migration files and model snapshot

--- TondForooshApi/Migrations/20250305200631_SpecifyProductPricePrecision.Designer.cs
+++ TondForooshApi/Migrations/20250305200631_SpecifyProductPricePrecision.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TondForooshApi.Models;
 
@@ -10,9 +11,11 @@ using TondForooshApi.Models;
 namespace TondForooshApi.Migrations
 {
     [DbContext(typeof(TFDbContext))]
-    partial class TFDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250305200631_SpecifyProductPricePrecision")]
+    partial class SpecifyProductPricePrecision
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
