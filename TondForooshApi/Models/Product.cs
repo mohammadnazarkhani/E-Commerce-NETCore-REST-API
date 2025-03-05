899f3518a615839a2b9b329772de13ae8264e3ee
mohammadnazarkhani <m.nazarkhani1380@gmail.com>
2025-03-05 21:30:43 +0330

    feat: Add Product model with properties for Id, Name, Description, Price, and ImageUrl

---
+++ TondForooshApi/Models/Product.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace TondForooshApi.Models;
+
+public class Product
+{
+    public long Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string? Description { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+    public string? ImageUrl { get; set; }
+}
