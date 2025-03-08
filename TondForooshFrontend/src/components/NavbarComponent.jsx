4b29d2e4091ca4f38d8576d019b1f7c9aac45d02
mohammadnazarkhani <m.nazarkhani1380@gmail.com>
2025-03-09 00:59:32 +0330

    feat: Refactor NavbarComponent to use return statement; add shine animation and update gradient styles in NavbarComponent CSS

--- TondForooshFrontend/src/components/NavbarComponent.jsx
+++ TondForooshFrontend/src/components/NavbarComponent.jsx
@@ -1,26 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import "../styles/NavbarComponent.css"; // Import the CSS file
 
-const NavbarComponent = () => (
-  <Navbar
-    dir="rtl"
-    variant="dark"
-    sticky="top"
-    expand="lg"
-    className="w-100 navbar-custom-bg"
-  >
-    <Container fluid>
-      <Navbar.Brand href="/" className="navbar-brand-custom">
-        تند‌فروش
-      </Navbar.Brand>
-      <Nav className="ms-auto">
-        <Nav.Link href="/" className="nav-link-custom">
-          خانه
-        </Nav.Link>
-      </Nav>
-    </Container>
-  </Navbar>
-);
+const NavbarComponent = () => {
+  return (
+    <Navbar
+      dir="rtl"
+      variant="dark"
+      sticky="top"
+      expand="lg"
+      className="w-100 navbar-custom-bg"
+    >
+      <Container fluid>
+        <Navbar.Brand href="/" className="navbar-brand-custom">
+          تند‌فروش
+        </Navbar.Brand>
+        <Nav className="ms-auto">
+          <Nav.Link href="/" className="nav-link-custom">
+            خانه
+          </Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+};
 
 export default NavbarComponent;
