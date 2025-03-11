c696e061171970ccfb663a41ba85916952c913e2
mohammadnazarkhani <m.nazarkhani1380@gmail.com>
2025-03-12 00:08:35 +0330

    feat: Add MainLayout styles and wrap Outlet in Container for improved layout structure

--- TondForooshFrontend/src/Layouts/MainLayout.jsx
+++ TondForooshFrontend/src/Layouts/MainLayout.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import NavbarComponent from "../components/NavbarComponent";
 import FooterComponent from "../components/FooterComponent";
 import { Outlet } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import './MainLayout.css'; // Import the CSS file for custom styles
 
 const MainLayout = () => {
   return (
     <>
       <NavbarComponent />
-      <Outlet />
+      <Container fluid className="main-content">
+        <Outlet />
+      </Container>
       <FooterComponent />
     </>
   );
