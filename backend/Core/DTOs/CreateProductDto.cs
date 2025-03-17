0fc83d20750126b93dd8e57979baf02893929d81
mohammadnazarkhani <122028480+mohammadnazarkhani@users.noreply.github.com>
2025-03-17 10:03:00 +0330

    feat: Implement Category entity and repository methods for category management

--- backend/Core/DTOs/CreateProductDto.cs
+++ backend/Core/DTOs/CreateProductDto.cs
@@ -6,5 +6,5 @@ public record class CreateProductDto(
     [Required(ErrorMessage = "Name is required")] string Name,
     string? Description,
     [Required(ErrorMessage = "Price is required")][Range(typeof(decimal), "1", "79228162514264337593543950335", ErrorMessage = "Price must be greater than 0")] decimal Price,
-    string? ImageUrl
-);
+    string? ImageUrl,
+    int CategoryId);
