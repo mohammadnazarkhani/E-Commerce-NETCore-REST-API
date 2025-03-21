037e8388e21ac6242d187dc772d36548b6e80777
mohammadnazarkhani <122028480+mohammadnazarkhani@users.noreply.github.com>
2025-03-21 14:08:21 +0330

    feat: Add ProductMapping class for mapping CreateProductDto and UpdateProductDto to Product entity

---
+++ backend/Core/Mapping/ProductMapping.cs
@@ -0,0 +1,34 @@
+using System;
+using Core.DTOs;
+using Core.Entities;
+
+namespace Core.Mapping;
+
+public static class ProductMapping
+{
+    public static Product ToEntity(this CreateProductDto createProductDto)
+    {
+        return new Product()
+        {
+            Name = createProductDto.Name,
+            Description = createProductDto.Description,
+            ImageUrl = createProductDto.ImageUrl,
+            CategoryId = createProductDto.CategoryId
+        };
+    }
+
+    public static Product ToEntity(this UpdateProductDto updateProductDto, long id)
+    {
+        return new Product()
+        {
+            Id = id,
+            Name = updateProductDto.Name,
+            Description = updateProductDto.Description,
+            Price = updateProductDto.Price,
+            ImageUrl = updateProductDto.ImageUrl,
+            CategoryId = updateProductDto.CategoryId
+        };
+    }
+
+    
+}
