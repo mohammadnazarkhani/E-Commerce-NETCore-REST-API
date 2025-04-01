17f2d3a22cf4ba992959567a74163c816b50d668
mohammadnazarkhani <122028480+mohammadnazarkhani@users.noreply.github.com>
2025-04-01 20:41:06 +0330

    feat: Add parent-child relationship to Category entity with ParentCategoryId and SubCategories properties

--- backend/Core/Entities/Category.cs
+++ backend/Core/Entities/Category.cs
@@ -9,6 +9,13 @@ public class Category : AuditableEntity<int>
     public required string Name { get; set; }
     public required string Description { get; set; }
 
+    #region Relationship
+    public int? ParentCategoryId { get; set; }
+    public Category? ParentCategory { get; set; }
+
+    public ICollection<Category> SubCategories { get; set; } = new List<Category>();
+
     // Navigation property to a collection of Products
     public ICollection<Product> Products { get; set; } = new HashSet<Product>();
+    #endregion
 }
