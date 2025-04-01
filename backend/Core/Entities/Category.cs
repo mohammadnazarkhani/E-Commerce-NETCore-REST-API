403f7ab0909078ef92b999e8ea06e94211d4b9b1
mohammadnazarkhani <122028480+mohammadnazarkhani@users.noreply.github.com>
2025-04-01 21:46:21 +0330

    fix: Change Description property in Category to be nullable

--- backend/Core/Entities/Category.cs
+++ backend/Core/Entities/Category.cs
@@ -7,7 +7,7 @@ namespace Core.Entities;
 public class Category : AuditableEntity<int>
 {
     public required string Name { get; set; }
-    public required string Description { get; set; }
+    public string? Description { get; set; }
 
     #region Relationship
     public int? ParentCategoryId { get; set; }
