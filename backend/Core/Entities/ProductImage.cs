b4f4567a15231f9de1a5b2318a7d45de44fd1d3f
mohammadnazarkhani <122028480+mohammadnazarkhani@users.noreply.github.com>
2025-04-01 20:31:25 +0330

    docs: Add XML documentation for ProductId property in ProductImage entity

--- backend/Core/Entities/ProductImage.cs
+++ backend/Core/Entities/ProductImage.cs
@@ -5,6 +5,9 @@ namespace Core.Entities;
 
 public class ProductImage : BaseMediaEntity
 {
+    /// <summary>
+    /// Relationship: One-to-One with Product
+    /// </summary>
     public int ProductId { get; set; }
     public Product? Product { get; set; }
 }
