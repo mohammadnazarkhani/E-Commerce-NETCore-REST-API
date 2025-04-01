63a67d5c48a4cc0814c684f230854e8f7aafc783
mohammadnazarkhani <122028480+mohammadnazarkhani@users.noreply.github.com>
2025-04-01 20:30:36 +0330

    refactor: Add decimal type specification for Price property in Product entity

--- backend/Core/Entities/Product.cs
+++ backend/Core/Entities/Product.cs
@@ -1,5 +1,6 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using Core.Entities.Base;
 using Core.Entities.Interfaces;
 
@@ -13,6 +14,7 @@ public class Product : AuditableEntity<long>, IVersionable
     [StringLength(2000)]
     public string? Description { get; set; } = string.Empty;
 
+    [Column(TypeName = "decimal(18, 2)")]
     public decimal Price { get; set; }
 
     [Range(0, int.MaxValue)]
