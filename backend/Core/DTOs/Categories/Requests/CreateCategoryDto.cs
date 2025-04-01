42ae5213567d8b52a5d23c0b43f13f12eefe5def
mohammadnazarkhani <122028480+mohammadnazarkhani@users.noreply.github.com>
2025-04-01 21:43:11 +0330

    refactor: Remove obsolete DTO classes for category requests and responses

--- backend/Core/DTOs/Categories/Requests/CreateCategoryDto.cs
+++
@@ -1,6 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-
-namespace Core.DTOs.Categories.Requests;
-
-public record CreateCategoryDto(
-    [Required(ErrorMessage = "Name is required")] string Name);
