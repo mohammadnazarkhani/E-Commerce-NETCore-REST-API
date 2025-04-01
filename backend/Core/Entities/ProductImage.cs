280f2736d1641bea632fefd092f7d8f1954501f2
mohammadnazarkhani <122028480+mohammadnazarkhani@users.noreply.github.com>
2025-04-01 20:08:01 +0330

    feat: Extend ProductImage to inherit from BaseFileEntity for enhanced file management

--- backend/Core/Entities/ProductImage.cs
+++ backend/Core/Entities/ProductImage.cs
@@ -1,11 +1,12 @@
 using System;
+using Core.Entities.Base;
 
 namespace Core.Entities;
 
 /// <summary>
 /// Represents an Image entity in the system, extending the base file entity with image-specific properties
 /// </summary>
-public class ProductImage
+public class ProductImage : BaseFileEntity
 {
     /// <summary>
     /// Url path to the thumbnail version of the image
