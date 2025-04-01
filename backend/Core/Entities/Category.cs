ac6cbb204d9fc22515da925c9047056af760ab16
mohammadnazarkhani <122028480+mohammadnazarkhani@users.noreply.github.com>
2025-04-01 20:33:02 +0330

    feat: Extend Category entity to inherit from AuditableEntity and add Description property

--- backend/Core/Entities/Category.cs
+++ backend/Core/Entities/Category.cs
@@ -1,12 +1,13 @@
 using System;
 using System.Collections.Generic;
+using Core.Entities.Base;
 
 namespace Core.Entities;
 
-public class Category
+public class Category : AuditableEntity<int>
 {
-    public int Id { get; set; }
     public required string Name { get; set; }
+    public required string Description { get; set; }
 
     // Navigation property to a collection of Products
     public ICollection<Product> Products { get; set; } = new HashSet<Product>();
