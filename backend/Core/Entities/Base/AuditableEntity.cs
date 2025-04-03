f74775153fecba80e9fa53f4333e0a9adb7ad7d6
mohammadnazarkhani <122028480+mohammadnazarkhani@users.noreply.github.com>
2025-04-03 11:06:32 +0330

    fix: Initialize CreatedAt property to current UTC time in AuditableEntity

--- backend/Core/Entities/Base/AuditableEntity.cs
+++ backend/Core/Entities/Base/AuditableEntity.cs
@@ -7,7 +7,7 @@ namespace Core.Entities.Base;
 public class AuditableEntity<TId> : EntityBase<TId>, IAuditableEntity, IEntityState
 {
 
-    public DateTime CreatedAt { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public DateTime? UpdatedAt { get; set; }
     public bool IsDeleted { get; set; }
     public DateTime? DeletedAt { get; set; }
