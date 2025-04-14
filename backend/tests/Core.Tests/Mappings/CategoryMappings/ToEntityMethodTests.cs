be8d855e54e74ff9c516b2563e5712e1864273e6
mohammadnazarkhani <122028480+mohammadnazarkhani@users.noreply.github.com>
2025-04-14 10:33:13 +0330

    test: add unit tests for mapping CreateCategoryDto to Category entity

---
+++ backend/tests/Core.Tests/Mappings/CategoryMappings/ToEntityMethodTests.cs
@@ -0,0 +1,54 @@
+using System;
+using Core.DTOs.Category;
+using Core.Entities;
+using Core.Mappings;
+
+namespace Core.Tests.Mappings.CategoryMappings;
+
+public class ToEntityMethodTests
+{
+
+    [Fact]
+    public void ToEntity_CanMapToEntityWithoutParent_WhenProvidedValidCreateCategoryDtoWithoutParentId()
+    {
+        // Arrange
+        CreateCategoryDto createCategoryDto = new()
+        {
+            Name = "cat",
+            Description = "desc"
+        };
+
+        // Act
+        Category category = createCategoryDto.ToEntity();
+
+        // Assert
+        Assert.NotNull(category);
+        Assert.Equal("cat", category.Name);
+        Assert.Equal("desc", category.Description);
+        Assert.Null(category.ParentCategoryId);
+    }
+
+    [Fact]
+    public void ToEntity_CanMapToEntityWithParentId_WhenProvidedValidCreateCategoryDtoWithParentId()
+    {
+        // Arrange
+        CreateCategoryDto createCategoryDto = new()
+        {
+            Name = "cat",
+            Description = "desc",
+            ParentCategoryId = 1
+        };
+
+        // Act
+        Category category = createCategoryDto.ToEntity();
+
+        // Assert
+        Assert.NotNull(category);
+        Assert.Equal("cat", category.Name);
+        Assert.Equal("desc", category.Description);
+        Assert.NotNull(category.ParentCategoryId);
+        Assert.Equal(1, category.ParentCategoryId);
+    }
+
+    
+}
