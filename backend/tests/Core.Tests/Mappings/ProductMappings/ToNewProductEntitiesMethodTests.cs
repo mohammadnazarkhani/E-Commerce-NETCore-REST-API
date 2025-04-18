84421b3bb401dba655b08222c50e1014dd39a51e
mohammadnazarkhani <122028480+mohammadnazarkhani@users.noreply.github.com>
2025-04-18 08:58:59 +0330

    test: add unit test for ToNewProductEntities method to handle null Image property

--- backend/tests/Core.Tests/Mappings/ProductMappings/ToNewProductEntitiesMethodTests.cs
+++ backend/tests/Core.Tests/Mappings/ProductMappings/ToNewProductEntitiesMethodTests.cs
@@ -15,4 +15,23 @@ public class ToNewProductEntitiesMethodTests
         // Act & Assert
         var exception = Assert.Throws<ArgumentNullException>(() => createProductDto.ToNewProductEntities());
     }
+
+    [Fact]
+    public void ToNewProductEntities_ThrowsArgumentNullException_WhenNullImageFileProvided()
+    {
+        // Arrange
+        CreateProductDto createProductDto = new()
+        {
+            Name = "product",
+            Description = "description",
+            Price = 100M,
+            CategoryId = 1,
+            ImageName = "name",
+            Image = null!,
+            StockQuantity = 0
+        };
+
+        // Act & Assert
+        var exception = Assert.Throws<ArgumentNullException>(() => createProductDto.ToNewProductEntities());
+    }
 }
