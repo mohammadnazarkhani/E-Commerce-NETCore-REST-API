bce002839b42641c04c77ec0a39b57290b91ed22
mohammadnazarkhani <122028480+mohammadnazarkhani@users.noreply.github.com>
2025-04-21 21:41:42 +0330

    test: add unit test for ToProductDetailsDto method to verify mapping from Product entity to ProductDetailsDto

--- backend/tests/Core.Tests/Mappings/ProductMappings/ToProductDetailsDtoMethodTests.cs
+++ backend/tests/Core.Tests/Mappings/ProductMappings/ToProductDetailsDtoMethodTests.cs
@@ -1,6 +1,9 @@
 using System;
+using Core.DTOs.Category;
+using Core.DTOs.Product;
 using Core.Entities;
 using Core.Mappings;
+using Microsoft.AspNetCore.Http.Features.Authentication;
 
 namespace Core.Tests.Mappings.ProductMappings;
 
@@ -14,5 +17,43 @@ public class ToProductDetailsDtoMethodTests
 
         // Act & Assert
         var exception = Assert.Throws<ArgumentNullException>(() => product.ToProductDetailsDto());
+    }
+
+    [Fact]
+    public void ToProductDetailsDto_CanMap_ProductEntityToDto()
+    {
+        // Arrange
+        Category category = new()
+        {
+            Id = 1,
+            Name = "cat1"
+        };
+        Product product = new Product()
+        {
+            Id = 1,
+            Name = "product",
+            Description = "description",
+            Price = 100M,
+            StockQuantity = 1,
+            Category = category,
+            CategoryId = category.Id
+        };
+
+        // Act
+        ProductDetailsDto productDetailsDto = product.ToProductDetailsDto();
+
+        // Assert - the returned dto obj not null
+        Assert.NotNull(productDetailsDto);
+        // Assert - the properties maped correctly
+        Assert.Equal(1, productDetailsDto.Id);
+        Assert.Equal("product", productDetailsDto.Name);
+        Assert.Equal("description", productDetailsDto.Description);
+        Assert.Equal(100M, productDetailsDto.Price);
+        Assert.Equal(1, productDetailsDto.StockQuantity);
+        Assert.Equal(Guid.Empty, productDetailsDto.MainImageId);
+        // Assert the category chain 
+        Stack<CategoryDto> categories = new Stack<CategoryDto>();
+        categories.Push(category.ToDto());
+        Assert.Equal(categories, productDetailsDto.Categories);
     }
 }
