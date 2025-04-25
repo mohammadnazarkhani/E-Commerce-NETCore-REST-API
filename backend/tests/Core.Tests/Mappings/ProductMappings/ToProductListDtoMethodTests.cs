e8849b013b839d974deb4c89143c5db55f0f31e0
mohammadnazarkhani <122028480+mohammadnazarkhani@users.noreply.github.com>
2025-04-25 16:48:20 +0330

    test: added test case testing ability of the ToProductListDto method to map Product entity to ProductListDto

--- backend/tests/Core.Tests/Mappings/ProductMappings/ToProductListDtoMethodTests.cs
+++ backend/tests/Core.Tests/Mappings/ProductMappings/ToProductListDtoMethodTests.cs
@@ -1,4 +1,5 @@
 using System;
+using Core.DTOs.Product;
 using Core.Entities;
 using Core.Mappings;
 
@@ -7,12 +8,36 @@ namespace Core.Tests.Mappings.ProductMappings;
 public class ToProductListDtoMethodTests
 {
     [Fact]
-    public void ToProductLIstDto_ShouldThrowArgumentNullException_WhenProductIsNull()
+    public void ToProductListDto_ShouldThrowArgumentNullException_WhenProductIsNull()
     {
         // Arrange
         Product product = null!;
 
         // Act & Assert
         var exception = Assert.Throws<ArgumentNullException>(() => product.ToProductListDto());
+    }
+
+    [Fact]
+    public void ToProductListDto_CanMapProductEntityToProductListDtoCurrectly()
+    {
+        // Arrange
+        Guid imageId = Guid.NewGuid();
+        Product product = new()
+        {
+            Id = 1,
+            Name = "prod",
+            Price = 100M,
+            MainImage = new ProductImage { Id = imageId }
+        };
+
+        // Act
+        ProductListDto dto = product.ToProductListDto();
+
+        // Assert
+        Assert.NotNull(dto);
+        Assert.Equal(1, dto.Id);
+        Assert.Equal("prod", dto.Name);
+        Assert.Equal(100M, dto.Price);
+        Assert.Equal(imageId, dto.MainImageId);
     }
 }
