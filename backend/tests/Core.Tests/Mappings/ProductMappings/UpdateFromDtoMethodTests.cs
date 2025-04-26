58940e9e2783197f711b54398f7af1092ffe7b54
mohammadnazarkhani <122028480+mohammadnazarkhani@users.noreply.github.com>
2025-04-26 09:17:05 +0330

    test: add initial product and category instances to UpdateFromDtoMethodTests

--- backend/tests/Core.Tests/Mappings/ProductMappings/UpdateFromDtoMethodTests.cs
+++ backend/tests/Core.Tests/Mappings/ProductMappings/UpdateFromDtoMethodTests.cs
@@ -6,4 +6,18 @@ namespace Core.Tests.Mappings.ProductMappings;
 
 public class UpdateFromDtoMethodTests
 {
+        Category category = new()
+    {
+        Id = 1,
+        Name = "cat1"
+    };
+
+    Product product = new()
+    {
+        Id = 1,
+        Name = "oldName",
+        Description = "oldDescription",
+        StockQuantity = 0,
+        Price = 100M,
+    };
 }
