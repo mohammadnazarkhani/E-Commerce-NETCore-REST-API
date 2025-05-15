9b1da1ff5287d4d69713400da2028f26eaf9e986
mohammadnazarkhani <122028480+mohammadnazarkhani@users.noreply.github.com>
2025-05-15 19:07:15 +0330

    feat: add ProductImageConfiguration for entity relationships

---
+++ backend/src/Infrastructure/Data/Configurations/ProductImageConfiguration.cs
@@ -0,0 +1,18 @@
+using System;
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Data.Configurations;
+
+public class ProductImageConfiguration : IEntityTypeConfiguration<ProductImage>
+{
+    public void Configure(EntityTypeBuilder<ProductImage> builder)
+    {
+        // Relations
+        builder.HasOne(pi => pi.Product)
+            .WithOne(p => p.MainImage)
+            .HasForeignKey<ProductImage>(pi => pi.ProductId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
