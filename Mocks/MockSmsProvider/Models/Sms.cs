54902335f5c7c1ae921a3d106c80c61aeb82bf9c
mohammadnazarkhani <122028480+mohammadnazarkhani@users.noreply.github.com>
2025-05-18 11:25:04 +0330

    feat: enhance Inbox, Sms, and User models with relation properties

--- Mocks/MockSmsProvider/Models/Sms.cs
+++ Mocks/MockSmsProvider/Models/Sms.cs
@@ -6,4 +6,11 @@ public class Sms
 {
     public required Guid Id { get; set; }
     public required string Message { get; set; }
+
+    // Relations
+    public Guid InboxId { get; set; }
+    public Inbox? Inbox { get; set; }
+
+    public required string SenderId { get; set; }
+    public User? Sender { get; set; }
 }
--- Mocks/MockSmsProvider/Models/User.cs
+++ Mocks/MockSmsProvider/Models/User.cs
@@ -6,4 +6,9 @@ namespace MockSmsProvider.Models;
 public class User
 {
     public required string Id { get; set; }
+
+    // Relations
+    public Inbox? Inbox { get; set; }
+
+    public List<Sms> SentMessages { get; set; } = new List<Sms>();
 }
