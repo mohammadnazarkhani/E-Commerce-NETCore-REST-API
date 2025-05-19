bfb8cdb8fdfbdec6e54041deeb2d4ac5a6e9c080
mohammadnazarkhani <122028480+mohammadnazarkhani@users.noreply.github.com>
2025-05-19 07:24:39 +0330

    feat: add SmsConfiguration for entity configuration of Sms model

---
+++ Mocks/MockSmsProvider/Data/Configurations/SmsConfiguration.cs
@@ -0,0 +1,21 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MockSmsProvider.Models;
+
+namespace MockSmsProvider.Data.Configurations;
+
+public class SmsConfiguration : IEntityTypeConfiguration<Sms>
+{
+    public void Configure(EntityTypeBuilder<Sms> builder)
+    {
+        // Relations
+        builder.HasOne(s => s.Inbox)
+            .WithMany(i => i.Messages)
+            .HasForeignKey(s => s.InboxId);
+
+        builder.HasOne(s => s.Sender)
+            .WithMany(sender => sender.SentMessages)
+            .HasForeignKey(s => s.SenderId);
+    }
+}
