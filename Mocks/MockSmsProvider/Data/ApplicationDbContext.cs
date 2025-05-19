0bd4eacdc5066ce5c98855141dff9f08d7e3964e
mohammadnazarkhani <122028480+mohammadnazarkhani@users.noreply.github.com>
2025-05-19 07:26:06 +0330

    feat: apply entity configurations from assembly in ApplicationDbContext

--- Mocks/MockSmsProvider/Data/ApplicationDbContext.cs
+++ Mocks/MockSmsProvider/Data/ApplicationDbContext.cs
@@ -20,5 +20,7 @@ public class ApplicationDbContext : DbContext
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
     }
 }
