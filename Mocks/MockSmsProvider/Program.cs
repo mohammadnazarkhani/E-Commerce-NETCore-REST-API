ce60d0945d01bfcf4a9e2967f6c9ba84d9b72c2b
mohammadnazarkhani <122028480+mohammadnazarkhani@users.noreply.github.com>
2025-05-20 16:47:38 +0330

    feat: configure ApplicationDbContext with SQL Server connection in Program.cs

--- Mocks/MockSmsProvider/Program.cs
+++ Mocks/MockSmsProvider/Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using MockSmsProvider.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDbContext>(opt =>
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
 
 var app = builder.Build();
 
