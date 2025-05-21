540afaadc9ac6090de5983bb246e12a003ee2007
mohammadnazarkhani <122028480+mohammadnazarkhani@users.noreply.github.com>
2025-05-21 09:38:01 +0330

    feat: implement SmsServices for sending SMS messages and managing inbox interactions

---
+++ Mocks/MockSmsProvider/Services/SmsServices.cs
@@ -0,0 +1,43 @@
+using System;
+using Microsoft.AspNetCore.SignalR.Protocol;
+using Microsoft.EntityFrameworkCore;
+using MockSmsProvider.Data;
+using MockSmsProvider.Models;
+using MockSmsProvider.Services.Base;
+
+namespace MockSmsProvider.Services;
+
+public class SmsServices : ServiceBase
+{
+    protected InboxServices _inboxServices;
+
+    public SmsServices(ApplicationDbContext context, InboxServices inboxServices) : base(context)
+    {
+        _inboxServices = inboxServices;
+    }
+
+    public async Task<bool> SendSms(string senderId, string userId, string message)
+    {
+        try
+        {
+            var receiverInbox = await _inboxServices.GetUserInbox(userId);
+
+            Sms sms = new()
+            {
+                Id = new Guid(),
+                Message = message,
+                InboxId = receiverInbox.Id,
+                SenderId = senderId
+            };
+
+            receiverInbox.Messages.Add(sms);
+            await _context.SaveChangesAsync();
+        }
+        catch (System.Exception)
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
