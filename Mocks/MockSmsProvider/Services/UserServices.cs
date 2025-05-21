75ce955b621876556fd69b35d64a6edc5aceb55c
mohammadnazarkhani <122028480+mohammadnazarkhani@users.noreply.github.com>
2025-05-21 09:39:42 +0330

    fix: ensure user account is saved correctly during sign-in process

--- Mocks/MockSmsProvider/Services/UserServices.cs
+++ Mocks/MockSmsProvider/Services/UserServices.cs
@@ -26,6 +26,7 @@ public class UserServices : ServiceBase
                 Id = id
             };
             await _context.Users.AddAsync(userAcc);
+            await _context.SaveChangesAsync();
         }
         catch (Exception)
         {
