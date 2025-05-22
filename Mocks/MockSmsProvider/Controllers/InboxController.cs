fccdf1437cdcfa42ac1200e40fe1366a4424544f
mohammadnazarkhani <122028480+mohammadnazarkhani@users.noreply.github.com>
2025-05-22 18:19:11 +0330

    fix: initialize ApplicationDbContext in InboxController for data access

--- Mocks/MockSmsProvider/Controllers/InboxController.cs
+++ Mocks/MockSmsProvider/Controllers/InboxController.cs
@@ -1,9 +1,17 @@
 using Microsoft.AspNetCore.Mvc;
+using MockSmsProvider.Data;
 
 namespace MockSmsProvider.Controllers
 {
     public class InboxController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public InboxController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         // GET: InboxController
         public ActionResult Index(string userId)
         {
