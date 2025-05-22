1f5106fa34666b9aabfaa177df79616ec15e51be
mohammadnazarkhani <122028480+mohammadnazarkhani@users.noreply.github.com>
2025-05-22 18:25:57 +0330

    refactor: remove unused ApplicationDbContext initialization in InboxController

--- Mocks/MockSmsProvider/Controllers/InboxController.cs
+++ Mocks/MockSmsProvider/Controllers/InboxController.cs
@@ -1,17 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
 using MockSmsProvider.Data;
+using MockSmsProvider.Services;
 
 namespace MockSmsProvider.Controllers
 {
     public class InboxController : Controller
     {
-        private ApplicationDbContext _context;
-
-        public InboxController(ApplicationDbContext context)
-        {
-            _context = context;
-        }
-
         // GET: InboxController
         public ActionResult Index(string userId)
         {
