2aa782252157750734e478f14929459e9b26e92e
mohammadnazarkhani <122028480+mohammadnazarkhani@users.noreply.github.com>
2025-05-22 18:27:00 +0330

    fix: initialize InboxServices in InboxController and update Index view model

--- Mocks/MockSmsProvider/Controllers/InboxController.cs
+++ Mocks/MockSmsProvider/Controllers/InboxController.cs
@@ -6,6 +6,13 @@ namespace MockSmsProvider.Controllers
 {
     public class InboxController : Controller
     {
+        private InboxServices _inboxServices;
+
+        public InboxController(InboxServices inboxServices)
+        {
+            _inboxServices = inboxServices;
+        }
+
         // GET: InboxController
         public ActionResult Index(string userId)
         {
