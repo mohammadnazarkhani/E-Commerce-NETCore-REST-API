0c60e9c59aebc8beeb83e26ef4cf93d1bfced5b1
mohammadnazarkhani <122028480+mohammadnazarkhani@users.noreply.github.com>
2025-05-22 18:10:03 +0330

    fix: update HomeController constructor to include ApplicationDbContext for data access

--- Mocks/MockSmsProvider/Controllers/HomeController.cs
+++ Mocks/MockSmsProvider/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using MockSmsProvider.Data;
 using MockSmsProvider.Models;
 
 namespace MockSmsProvider.Controllers;
@@ -7,10 +8,12 @@ namespace MockSmsProvider.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private ApplicationDbContext _context;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
     public IActionResult Index()
