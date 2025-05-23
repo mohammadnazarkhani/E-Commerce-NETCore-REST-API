21e425a5a643150d7bdd5291a297842035c8b39d
mohammadnazarkhani <122028480+mohammadnazarkhani@users.noreply.github.com>
2025-05-23 09:52:03 +0330

    fix: enhance documentation for UserNotFoundException class

--- Mocks/MockSmsProvider/Common/Exceptions/UserNotFoundException.cs
+++ Mocks/MockSmsProvider/Common/Exceptions/UserNotFoundException.cs
@@ -2,8 +2,19 @@ using System;
 
 namespace MockSmsProvider.Common.Exceptions;
 
+/// <summary>
+/// Represents an exception that is thrown when a requested user cannot be found in the system.
+/// </summary>
+/// <remarks>
+/// This exception is typically thrown when attempting to perform operations on a user
+/// that does not exist in the database or system context.
+/// </remarks>
 public class UserNotFoundException : Exception
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="UserNotFoundException"/> class with a specified error message.
+    /// </summary>
+    /// <param name="message">The message that describes the error. This message is passed to the base Exception class.</param>
     public UserNotFoundException(string? message) : base(message)
     {
     }
