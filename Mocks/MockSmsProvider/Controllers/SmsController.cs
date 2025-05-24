5476b9550bfabb41186ee6f76e384a73aed2dfbd
mohammadnazarkhani <122028480+mohammadnazarkhani@users.noreply.github.com>
2025-05-24 10:44:13 +0330

    docs: added xml documentation for sms api controller

--- Mocks/MockSmsProvider/Controllers/SmsController.cs
+++ Mocks/MockSmsProvider/Controllers/SmsController.cs
@@ -5,6 +5,9 @@ using MockSmsProvider.Services.Interfaces;
 
 namespace MockSmsProvider.Controllers
 {
+    /// <summary>
+    /// Api cotroller for sms related actions
+    /// </summary>
     [Route("api/[controller]")]
     [ApiController]
     public class SmsController : ControllerBase
@@ -16,6 +19,11 @@ namespace MockSmsProvider.Controllers
             _smsService = smsService;
         }
 
+        /// <summary>
+        /// Api endpoint for sending sms for another user
+        /// </summary>
+        /// <param name="sendSmsDto">DTO for sending message. Includes: id of sender, id of receiver and the message itself.</param>
+        /// <returns>Returns SendSmsResultDto DTO which continas a boolean value indication success and a error message if needed.</returns>
         [HttpPost]
         public async Task<ActionResult<SendSmsResultDto>> SendSms([FromBody] SendSmsDto sendSmsDto)
         {
