a2019132c486f466553011592d912dfe3f6a950a
mohammadnazarkhani <122028480+mohammadnazarkhani@users.noreply.github.com>
2025-05-25 15:24:45 +0330

    docs: added xml docs for User entity class

--- Mocks/MockSmsProvider/Models/User.cs
+++ Mocks/MockSmsProvider/Models/User.cs
@@ -3,8 +3,14 @@ using System.ComponentModel.DataAnnotations;
 
 namespace MockSmsProvider.Models;
 
+/// <summary>
+/// User entity representing the users which will be identified with phone number/company name.
+/// </summary>
 public class User
 {
+    /// <summary>
+    /// Id property which represents user's phone number or company name
+    /// </summary>
     public required string Id { get; set; }
 
     // Relations
