f2a7a43ced0b604fcaa702056176ed4f84cc7f47
mohammadnazarkhani <122028480+mohammadnazarkhani@users.noreply.github.com>
2025-06-02 07:06:22 +0330

    feat: add User model with properties for FirstName, LastName, EmailAddress, Password, PhoneNumber, and DateOfBirth

---
+++ Mocks/FakeEmailGateway/Models/User.cs
@@ -0,0 +1,21 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace FakeEmailGateway.Models;
+
+public class User : Base.EntityBase
+{
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    [Required(ErrorMessage = "Email address is required.")]
+    [EmailAddress(ErrorMessage = "Invalid email address format.")]
+    public required string EmailAddress { get; set; }
+    [Required(ErrorMessage = "Password is required.")]
+    [MinLength(6, ErrorMessage = "Password must be at least 6 characters long.")]
+    [DataType(DataType.Password)]
+    public required string Password { get; set; }
+    [DataType(DataType.PhoneNumber)]
+    public string? PhoneNumber { get; set; }
+    [DataType(DataType.Date)]
+    public DateTime? DateOfBirth { get; set; }
+}
