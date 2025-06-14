73b9659006606ede3eb33dda937459861b7c23ed
mohammadnazarkhani <122028480+mohammadnazarkhani@users.noreply.github.com>
2025-06-14 12:21:17 +0330

    Add XML documentation for City and Province classes
    Enhanced code readability by adding XML documentation comments to the `City` and `Province` classes. The comments summarize each class's purpose and detail their relationships within the e-commerce system, providing useful context for developers.

--- ECommerceSln/ECommerce.RestAPI/Entities/City.cs
+++ ECommerceSln/ECommerce.RestAPI/Entities/City.cs
@@ -3,6 +3,14 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ECommerce.RestAPI.Entities
 {
+    /// <summary>
+    /// Represents a city within the e-commerce system for user addresses.
+    /// Has a name and establishes a relationship with a province.
+    /// </summary>
+    /// <remarks>
+    /// Inherits from <see cref="AuditableEntityBase"/> to track creation and modification dates.
+    /// Has a required name field and a relationship with the <see cref="Province"/> entity.
+    /// </remarks>
     public class City : AuditableEntityBase
     {
         [Length(2, 255)]
--- ECommerceSln/ECommerce.RestAPI/Entities/Province.cs
+++ ECommerceSln/ECommerce.RestAPI/Entities/Province.cs
@@ -5,6 +5,14 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ECommerce.RestAPI.Entities
 {
+    /// <summary>
+    /// Represents a province within the e-commerce system for user addresses.
+    /// It contains the name of the province and establishes a relationship with a city.
+    /// </summary>
+    /// <remarks>
+    /// Inherits from <see cref="AuditableEntityBase"/> to track creation and modification dates.
+    /// Relationships are established with the <see cref="City"/> entity.
+    /// </remarks>
     public class Province : AuditableEntityBase
     {
         [Length(2,50)]
