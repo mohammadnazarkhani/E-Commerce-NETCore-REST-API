d4bf9e0b9e5ef27d5e086e5e1e87c456989c18be
mohammadnazarkhani <122028480+mohammadnazarkhani@users.noreply.github.com>
2025-06-15 21:22:13 +0330

    Add Rating enum and update Review and User classes
    Introduced a new `Rating` enum in `Rating.cs` to represent review scores from 1 to 5. Updated `Review.cs` to include a `RatingScore` property of type `Rating`, allowing for structured rating assignments. Made minor updates to `User.cs` for consistency and clarity, ensuring it maintains its role in the e-commerce system with necessary properties and auditing capabilities.

---
+++ ECommerceSln/ECommerce.RestAPI/Entities/Review.cs
@@ -0,0 +1,26 @@
+﻿using ECommerce.RestAPI.Entities.Base;
+using ECommerce.RestAPI.Entities.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace ECommerce.RestAPI.Entities
+{
+    /// <summary>
+    /// Represents a review entry in the system.
+    /// </summary>
+    public class Review : AuditableEntityBase
+    {
+        /// <summary>
+        /// Gets or sets the review comment text.
+        /// </summary>
+        public required string Comment { get; set; }
+
+        /// <summary>
+        /// Gets or sets the rating score for this review.
+        /// </summary>
+        /// <remarks>
+        /// The rating is stored as an enum value from 1 to 5 stars.
+        /// </remarks>
+        [EnumDataType(typeof(Rating))]
+        public Rating RatingScore { get; set; }
+    }
+}
---
+++ ECommerceSln/ECommerce.RestAPI/Entities/User.cs
@@ -0,0 +1,26 @@
+﻿using ECommerce.RestAPI.Entities.Base;
+using ECommerce.RestAPI.Entities.Interfaces;
+using Microsoft.AspNetCore.Identity;
+
+namespace ECommerce.RestAPI.Entities
+{
+    /// <summary>
+    /// Represents a user in the e-commerce system.
+    /// Has properties for first name, last name, and national code.
+    /// </summary>
+    /// <remarks>
+    /// Inherits from <see cref="IdentityUser{TKey}"/> with a <see cref="Guid"/> key type.
+    /// Also implements <see cref="IAuditableEntity"/> to track creation and modification dates.
+    /// </remarks>
+    public class User : IdentityUser<Guid>, IAuditableEntity
+    {
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public string? NationalCode { get; set; }
+
+        // Auditable properties
+        public required Guid Id { get; set; }
+        public required DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime? LastModifiedAt { get; set; }
+    }
+}
