29ddddd4faf878161e9281641072d3fda385e464
mohammadnazarkhani <122028480+mohammadnazarkhani@users.noreply.github.com>
2025-06-16 21:06:47 +0330

    Add OrderStatus enum and update Order class
    Introduces a new `OrderStatus` enumeration in the
    `ECommerce.RestAPI.Entities.Enums` namespace to define
    various order statuses. Updates the `Order` class in
    the `ECommerce.RestAPI.Entities` namespace to include
    a required `Status` property of type `OrderStatus`.

---
+++ ECommerceSln/ECommerce.RestAPI/Entities/Order.cs
@@ -0,0 +1,10 @@
+﻿using ECommerce.RestAPI.Entities.Base;
+using ECommerce.RestAPI.Entities.Enums;
+
+namespace ECommerce.RestAPI.Entities
+{
+    public class Order : AuditableEntityBase
+    {
+        public required OrderStatus Status { get; set; }
+    }
+}
