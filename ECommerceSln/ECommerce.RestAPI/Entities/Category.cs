99147e79257b01f514fe8308ea4e1c1e1e509198
mohammadnazarkhani <122028480+mohammadnazarkhani@users.noreply.github.com>
2025-06-16 20:58:31 +0330

    Add Category class with required Name property
    Introduces a new `Category` class in the `ECommerce.RestAPI.Entities` namespace. The class inherits from `AuditableEntityBase` and includes a required `Name` property of type `string`.

---
+++ ECommerceSln/ECommerce.RestAPI/Entities/Category.cs
@@ -0,0 +1,9 @@
+﻿using ECommerce.RestAPI.Entities.Base;
+
+namespace ECommerce.RestAPI.Entities
+{
+    public class Category : AuditableEntityBase
+    {
+        public required string Name { get; set; }
+    }
+}
