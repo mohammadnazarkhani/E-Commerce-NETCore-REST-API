b779f2b980790a9621ca330eafc73fdc8faeab8d
mohammadnazarkhani <122028480+mohammadnazarkhani@users.noreply.github.com>
2025-06-16 20:59:33 +0330

    Add Product class to ECommerce.RestAPI.Entities
    Introduced a new `Product` class that inherits from
    `AuditableEntityBase`. The class features properties for
    `Name` (required), `Description`, `Price`, and
    `StockQuantity` to support product management.

---
+++ ECommerceSln/ECommerce.RestAPI/Entities/Product.cs
@@ -0,0 +1,12 @@
+﻿using ECommerce.RestAPI.Entities.Base;
+
+namespace ECommerce.RestAPI.Entities
+{
+    public class Product : AuditableEntityBase
+    {
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+        public int StockQuantity { get; set; }
+    }
+}
