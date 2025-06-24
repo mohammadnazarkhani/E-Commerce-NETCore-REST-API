14b461df20d4e5a3036f96dcfc9217dd12e24a60
mohammadnazarkhani <122028480+mohammadnazarkhani@users.noreply.github.com>
2025-06-24 21:05:30 +0330

    Added base entity configuration for AuditableEntityBase

---
+++ ECommerceSln/ECommerce.RestAPI/Data/Configurations/BaseEntityConfigurations.cs
@@ -0,0 +1,17 @@
+﻿using ECommerce.RestAPI.Entities.Base;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ECommerce.RestAPI.Data.Configurations
+{
+    public class AuditableEntityConfiguration : IEntityTypeConfiguration<AuditableEntityBase>
+    {
+        public void Configure(EntityTypeBuilder<AuditableEntityBase> builder)
+        {
+            builder.UseTpcMappingStrategy();
+
+            builder.Property(e => e.CreatedAt)
+                .HasDefaultValue("GETUTCDATE()");
+        }
+    }
+}
