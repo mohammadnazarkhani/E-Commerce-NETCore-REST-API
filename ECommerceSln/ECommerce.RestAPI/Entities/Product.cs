0578301a027f563ad7ccd1ef131533a16448d1d5
Mohammad Nazarkhani <m.nazarkhani1380@gmail.com>
2025-07-03 21:33:39 +0330

    feat(entities): add validation attributes to Product and Question properties

--- ECommerceSln/ECommerce.RestAPI/Entities/Product.cs
+++ ECommerceSln/ECommerce.RestAPI/Entities/Product.cs
@@ -1,23 +1,34 @@
-﻿using ECommerce.RestAPI.Entities.Base;
+﻿using System.ComponentModel.DataAnnotations;
+using ECommerce.RestAPI.Entities.Base;
 
 namespace ECommerce.RestAPI.Entities
 {
     public class Product : AuditableEntityBase
     {
+        [Required]
+        [MaxLength(100)]
         public required string Name { get; set; }
+
+        [MaxLength(1000)]
         public string? Description { get; set; }
+
+        [Required]
         public decimal Price { get; set; }
+
+        [Required]
         public int StockQuantity { get; set; }
 
         // Relationships
-        public required Guid CategoryId { get; set; }
+        [Required]
+        public Guid CategoryId { get; set; }
         public Category Category { get; set; } = null!;
 
         public ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
 
         public ICollection<CartItem> CartItems { get; set; } = new List<CartItem>();
 
-        public required Guid VendorId { get; set; }
+        [Required]
+        public Guid VendorId { get; set; }
         public Vendor Vendor { get; set; } = null!;
 
         public ICollection<Review> Reviews { get; set; } = new List<Review>();
--- ECommerceSln/ECommerce.RestAPI/Entities/Question.cs
+++ ECommerceSln/ECommerce.RestAPI/Entities/Question.cs
@@ -1,17 +1,23 @@
-﻿using ECommerce.RestAPI.Entities.Base;
+﻿using System.ComponentModel.DataAnnotations;
+using ECommerce.RestAPI.Entities.Base;
 
 namespace ECommerce.RestAPI.Entities
 {
     public class Question : AuditableEntityBase
     {
+        [Required]
+        [MaxLength(500)]
         public required string Quest { get; set; }
+
         public string? Answer { get; set; }
 
         // Relationships
-        public required Guid UserId { get; set; }
+        [Required]
+        public Guid UserId { get; set; }
         public User User { get; set; } = null!;
 
-        public required Guid ProductId { get; set; }
+        [Required]
+        public Guid ProductId { get; set; }
         public Product Product { get; set; } = null!;
     }
 }
