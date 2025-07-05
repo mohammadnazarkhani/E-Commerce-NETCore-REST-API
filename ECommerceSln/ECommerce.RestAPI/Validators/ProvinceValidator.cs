f6e3d514516cf67c2a4fb710cc3d30fbbbec98fc
Mohammad Nazarkhani <m.nazarkhani1380@gmail.com>
2025-07-05 15:10:34 +0330

    feat(validator): enhance ProvinceValidator with additional validation messages

--- ECommerceSln/ECommerce.RestAPI/Validators/ProvinceValidator.cs
+++ ECommerceSln/ECommerce.RestAPI/Validators/ProvinceValidator.cs
@@ -9,10 +9,13 @@ namespace ECommerce.RestAPI.Validators
         {
             RuleFor(p => p.Name)
                 .NotEmpty()
+                .WithMessage("Province name is required.")
                 .Length(2, 50)
                 .WithMessage("Province name must be between 2 and 50 characters")
                 .Matches("^[\\p{L}\\s-]+$")
-                .WithMessage("Province name can only contain letters, spaces, and hyphens");
+                .WithMessage("Province name can only contain letters, spaces, and hyphens")
+                .MaximumLength(50)
+                .WithMessage("Province name cannot be longer than 50 characters.");
         }
     }
 }
