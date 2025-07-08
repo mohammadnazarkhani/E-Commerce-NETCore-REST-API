7368aa46016a68831245bd29cdd2185c64f43a89
Mohammad Nazarkhani <m.nazarkhani1380@gmail.com>
2025-07-08 22:41:07 +0330

    feat(config): configure PostgreSQL DbContext and add connection string in appsettings

--- ECommerceSln/ECommerce.RestAPI/Program.cs
+++ ECommerceSln/ECommerce.RestAPI/Program.cs
@@ -1,5 +1,16 @@
+using ECommerce.RestAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Configure PostgreSQL DbContext
+builder.Services.AddDbContext<ECommerceDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
+
+// Migrate and update database at startup
+app.MigrateAndSeedDatabase();
 
 app.MapGet("/", () => "Hello World!");
 
