27d8a3fa5e01b42277d7a67cf1819bfecee25982
Mohammad Nazarkhani <m.nazarkhani1380@gmail.com>
2025-07-16 06:43:34 +0330

    feat: add IReadSpecification interface for read operations

---
+++ ECommerceSln/ECommerce.RestAPI/Data/Specifications/IReadSpecefication.cs
@@ -0,0 +1,18 @@
+using System;
+using ECommerce.RestAPI.Entities.Interfaces;
+
+namespace ECommerce.RestAPI.Data.Specifications;
+
+/// <summary>
+/// Specification interface for read oprations
+/// </summary>
+/// <typeparam name="TEntity">Entity type</typeparam>
+public interface IReadSpecefication<TEntity> : ISpecification<TEntity> where TEntity : class, IEntity
+{
+    /// <summary>
+    /// Evaluates the specification against an entity
+    /// </summary>
+    /// <param name="entity">Entity to evaluate</param>
+    /// <returns>True if entity staisfies the specification</returns>
+    bool ISSatisfiedBy(TEntity entity);
+}
