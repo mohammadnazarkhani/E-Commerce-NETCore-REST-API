61dea8a217de6848490fb50f48c0c808e2c46a7f
Mohammad Nazarkhani <m.nazarkhani1380@gmail.com>
2025-07-20 13:20:44 +0330

    feat: add MigrateAndSeedDatabase method for database migration at startup

--- ECommerceSln/ECommerce.RestAPI/Data/Extensions/DataExtensions.cs
+++ ECommerceSln/ECommerce.RestAPI/Data/Extensions/DataExtensions.cs
@@ -2,7 +2,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace ECommerce.RestAPI.Data
+namespace ECommerce.RestAPI.Data.Extensions
 {
     public static class DataExtensions
     {
--- ECommerceSln/ECommerce.RestAPI/Program.cs
+++ ECommerceSln/ECommerce.RestAPI/Program.cs
@@ -1,4 +1,5 @@
 using ECommerce.RestAPI.Data;
+using ECommerce.RestAPI.Data.Extensions;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
