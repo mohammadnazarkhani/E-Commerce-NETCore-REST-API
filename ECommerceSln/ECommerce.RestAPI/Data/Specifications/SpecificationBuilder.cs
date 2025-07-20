6015b60086c19f238261309e9319e6c102878b5c
Mohammad Nazarkhani <m.nazarkhani1380@gmail.com>
2025-07-20 20:23:48 +0330

    feat: enhance SpecificationBuilder with additional methods for querying and grouping

--- ECommerceSln/ECommerce.RestAPI/Data/Specifications/SpecificationBuilder.cs
+++ ECommerceSln/ECommerce.RestAPI/Data/Specifications/SpecificationBuilder.cs
@@ -22,36 +22,67 @@ public class SpecificationBuilder<TEntity> where TEntity : class, IEntity
     private bool _asSplitQuery;
     private Expression<Func<TEntity, object>>? _groupBy;
 
+    /// <summary>
+    /// Adds a where clause to the specification.
+    /// </summary>
+    /// <param name="criteria">Filter criteria</param>
+    /// <returns>Current builder instance</returns>
     public SpecificationBuilder<TEntity> Where(Expression<Func<TEntity, bool>> criteria)
     {
         _criteria.Add(criteria);
         return this;
     }
 
+    /// <summary>
+    /// Adds an include expression for eager loading related entities.
+    /// </summary>
+    /// <param name="includeExpression">Include expression</param>
+    /// <returns>Current builder instance</returns>
     public SpecificationBuilder<TEntity> Include(Expression<Func<TEntity, object>> includeExpression)
     {
         _includes.Add(includeExpression);
         return this;
     }
 
+    /// <summary>
+    /// Adds an include string for eager loading related entities.
+    /// </summary>
+    /// <param name="includeString">Include string</param>
+    /// <returns>Current builder instance</returns>
     public SpecificationBuilder<TEntity> Include(string includeString)
     {
         _includeStrings.Add(includeString);
         return this;
     }
 
+    /// <summary>
+    /// Adds an order by expression to the specification.
+    /// </summary>
+    /// <param name="orderByExpression">Order by expression</param>
+    /// <returns>Current builder instance</returns>
     public SpecificationBuilder<TEntity> OrderBy(Expression<Func<TEntity, object>> orderByExpression)
     {
         _orderBy.Add(orderByExpression);
         return this;
     }
 
+    /// <summary>
+    /// Adds an order by descending expression to the specification.
+    /// </summary>
+    /// <param name="orderByDescExpression">Order by descending expression</param>
+    /// <returns>Current builder instance</returns>
     public SpecificationBuilder<TEntity> OrderByDescending(Expression<Func<TEntity, object>> orderByDescExpression)
     {
         _orderByDescending.Add(orderByDescExpression);
         return this;
     }
 
+    /// <summary>
+    /// Applies pagin to the specification.
+    /// </summary>
+    /// <param name="skip">Number of items to skip</param>
+    /// <param name="take">Number of items to take</param>
+    /// <returns>Current builder instance</returns>
     public SpecificationBuilder<TEntity> Paging(int skip, int take)
     {
         _skip = skip;
@@ -59,27 +90,46 @@ public class SpecificationBuilder<TEntity> where TEntity : class, IEntity
         return this;
     }
 
+    /// <summary>
+    /// Disables change tracking for the specification.
+    /// This method allows the query to be executed without tracking changes to the entities.
+    /// </summary>
+    /// <returns>Current builder instance</returns>
     public SpecificationBuilder<TEntity> AsNoTracking()
     {
         _asNoTracking = true;
         return this;
     }
 
+    /// <summary>
+    /// Enables split query for the specification.
+    /// </summary>
+    /// <returns>Current builder instance</returns>
     public SpecificationBuilder<TEntity> AsSplitQuery()
     {
         _asSplitQuery = true;
         return this;
     }
 
+    /// <summary>
+    /// Adds a group by expression to the specification.
+    /// This method allows grouping entities by a specified property.
+    /// </summary>
+    /// <param name="groupByExpression">Group by expression</param>
+    /// <returns>Current builder instance</returns>
     public SpecificationBuilder<TEntity> GroupBy(Expression<Func<TEntity, object>> groupByExpression)
     {
         _groupBy = groupByExpression;
         return this;
     }
 
+    /// <summary>
+    /// Builds the specification
+    /// </summary>
+    /// <returns>Built specification</returns>
     public ISpecification<TEntity> Build()
     {
-        return new BuiltSpecificcation<TEntity>(
+        return new BuiltSpecification<TEntity>(
             _criteria,
             _includes,
             _includeStrings,
